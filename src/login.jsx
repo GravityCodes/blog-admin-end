187460b22d98eeae4081e4d250e9e6b0b3a7823f
Johan <Johanotr@gmail.com>
2025-09-06 10:39:31 -0400

    WIP: Initial progress on login

--- src/login.jsx
+++ src/login.jsx
@@ -1,8 +1,31 @@
 export default function Login() {
+  
+  const formSubmit = async (formdata) => {
+    const email = formdata.get("email");
+    const password = formdata.get("password");
+
+    const request = fetch(import.meta.env.VITE_BLOG_LOGIN, {
+      method: "POST",
+      headers : {
+        "Content-Type" : ""
+      }
+    });
+  }
 
   return (
     <>
       <h1>Login</h1>
+      <form action={formSubmit}>
+        <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+        </div>
+        <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Log In</button>
+      </form>
     </>
   )
 }
